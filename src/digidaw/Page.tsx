25efe95c04c1f571e6c977452325bf52a1eed1e7
IZZUDIN AR RAFIQ <izzudin.ar26@gmail.com>
2024-09-04 15:57:38 +0700

    digidaw: finish desktop layout

--- src/digidaw/Page.tsx
+++ src/digidaw/Page.tsx
@@ -2,17 +2,18 @@ import DigidawNavbar from "@digidaw/Components/Navbar";
 import "@digidaw/Assets/digidaw.css";
 import { HiOutlineSquare3Stack3D } from "react-icons/hi2";
 import DigidawCardYellow from "@digidaw/Components/CardYellow";
-// import DigidawThreeWhiteCard from "@digidaw/Components/ThreeWhiteCard";
-// import DigidawPurpleCard from "@digidaw/Components/PurpleCard";
+import DigidawThreeWhiteCard from "@digidaw/Components/ThreeWhiteCard";
+import DigidawPurpleCard from "@digidaw/Components/PurpleCard";
 import DigidawIdentityGreenCard from "@digidaw/Components/IdentityGreenCard";
+import DigidawGreenCard from "@digidaw/Components/GreenCard";
 
 export default function DigidawPage() {
   return (
     <div className="w-full flex min-h-[100svh] bg-gradient-to-br from-digidaw-primary to-digidaw-secondary">
       <div className="container mx-auto px-5 lg:px-0 py-8">
         <DigidawNavbar />
-        <div className="grid grid-cols-2 gap-5 mt-10">
-          <div className="flex flex-col w-full">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 mt-10">
+          <div className="flex flex-col w-full justify-center">
             <h2 className="text-5xl text-white font-notoSans font-light tracking-wide">
               Experience the power of
             </h2>
@@ -49,13 +50,21 @@ export default function DigidawPage() {
             </div>
           </div>
           {/* Cards */}
-          <div className="flex relative w-full">
-            <div className="w-1/2">
+          <div className="hidden xl:flex relative w-full min-h-[700px] items-center justify-center">
+            <div className="w-1/2 scale-75 absolute left-7 top-0">
+              <DigidawPurpleCard />
+            </div>
+            <div className="w-1/2 scale-75 absolute right-7 top-0">
+              <DigidawGreenCard />
+            </div>
+            <div className="w-1/2 absolute left-0">
               <DigidawCardYellow />
             </div>
-            <div className="w-1/2">
+            <div className="w-1/2 absolute -right-10">
+              <DigidawThreeWhiteCard />
+            </div>
+            <div className="w-1/2 scale-90 absolute left-20 bottom-10">
               <DigidawIdentityGreenCard />
-              {/* <DigidawThreeWhiteCard /> */}
             </div>
           </div>
         </div>
